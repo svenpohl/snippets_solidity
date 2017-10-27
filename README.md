1f86a6907d2620c50bc289526a6e320169ae93db
svenpohl <sven@zen-project.de>
2017-10-27 11:57:40 +0200

    Add timesafe.sol

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # snippets_solidity
 Snippets for solidity (Ethereum)
 
+muenzwurf.sol:
 Last Live-Test (Solidity 0.4.16): Sat. 26. august 2017.
                                   Ethereum-Environment: Pairity 1.6.6
 
+timesafe.sol:
+Last Live-Test (Solidity 0.4.18): Fr. 27. october 2017
---
+++ timesafe.sol
@@ -0,0 +1,82 @@
+/*
+
+Timelock.sol - sven@derkryptonaer.de
+
+26.Okt.2017 - Version 1.0 - erstellt
+
+*/
+pragma solidity ^0.4.18;  
+
+contract timelock  
+{
+address public owner; 
+uint256 public timestamp_payout            = 0;
+uint256 public sekunden_bis_zur_auszahlung = 0;
+string  public meldung;
+
+
+//
+//
+// Konstruktor 
+//
+//
+function timelock ( uint _timestamp_payout ) public
+{
+owner            = msg.sender;
+timestamp_payout = _timestamp_payout;
+} // function timelock  
+
+
+
+
+//
+//
+// Einzahlung
+//
+//
+function () payable public  
+{
+meldung = "Einzahlung wurde gemacht!";
+} // ()
+
+
+
+
+
+//
+//
+// withdraw - Auszahlung
+//
+//
+function withdraw () public   
+{
+
+if ( (msg.sender == owner) )
+   {
+   
+   if (now >= timestamp_payout)
+      {
+      //
+      // Auszahlung
+      //
+      owner.transfer( this.balance );
+   
+      selfdestruct(owner); 
+      meldung = "OK!";
+      } else
+        {        
+        sekunden_bis_zur_auszahlung = timestamp_payout - now;
+        meldung = "Zeit noch nicht abgelaufen!";
+        }
+        
+   } // if ( (msg.sender == owner) )
+   else
+       {
+       meldung = "Falsche Adresse!";
+       }
+                   
+} // function withdraw ()
+
+
+} // END - contract timelock 
+
